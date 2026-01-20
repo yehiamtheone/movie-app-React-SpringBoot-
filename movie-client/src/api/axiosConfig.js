792f43b4a4da1974143129dc0c14784eb3a47800
yehiamtheone <yehiamknafo5757@gmail.com>
2026-01-20 14:58:29 +0200

    added render server to react

--- movie-client/src/api/axiosConfig.js
+++ movie-client/src/api/axiosConfig.js
@@ -5,7 +5,7 @@ import env from "react-dotenv";
 // console.log(`${import.meta.env.VITE_SPRING_BOOT}/api/v1`);
 
 const moviesApi = axios.create({
-  baseURL: "/api/v1",
+  baseURL: `${import.meta.env.VITE_SPRING_BOOT}/api/v1`,
   headers: {
     'Content-Type': 'application/json'
   }
